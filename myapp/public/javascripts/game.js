6a8e7db3905062a2071379b733ee9d634a4ad9b7
Jordy van der Tang <jordyvandertang@hotmail.com>
2018-12-04 13:47:13 +0100

    'functional' clicking

--- myapp/public/javascripts/game.js
+++ myapp/public/javascripts/game.js
@@ -1,7 +1,25 @@
+
+var boardSize = 8;
+var grid = new Array(boardSize);
+
+for(var i = 0 ;i < boardSize;i++){
+
+    grid[i] = new Array(boardSize);
+
+    for(var j = 0; j < boardSize;j++){
+        grid[i][j] = false;
+    }
+}
+
 $(document).ready(function(){
     $(".board-tile").click(function(){
-        alert($(this).attr('id'));
-        $(this).append("<img src='./images/bitmap.png' />")
-    }) 
+        var col = $(this).attr('id').substring(1);
+        
+        var row = $(this).parent().attr('id').substring(3);
+        if(!grid[col][row]){
+            $(this).append("<img src='./images/bitmap.png' />")
+            grid[col][row] = true;
+        }
+    })
 
 })
